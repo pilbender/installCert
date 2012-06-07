88127c09e5d97139f35b58f6e8959564fb7e11ae
Richard Scott Smith <scott@thescene.com>
2012-06-07 12:39:39 -0700

    Initial import of local cert install program.

---
+++ src/test/java/net/raescott/installcert/InstallCertTest.java
@@ -0,0 +1,35 @@
+package net.raescott.installcert;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple InstallCert.
+ */
+public class InstallCertTest
+		extends TestCase {
+
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public InstallCertTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(InstallCertTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testInstallCert() {
+		assertTrue(true);
+	}
+}
